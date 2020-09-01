97bd1f1f243b02c2b7e5f8fef12262a36f62ecd1
Kevin Deisz <kevin.deisz@gmail.com>
2020-09-01 17:04:54 -0400

    Initial commit

---
+++ lib/ragel/array.rb
@@ -0,0 +1,8 @@
+require "ragel/array/version"
+
+module Ragel
+  module Array
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ragel/array/version.rb
@@ -0,0 +1,5 @@
+module Ragel
+  module Array
+    VERSION = "0.1.0"
+  end
+end
